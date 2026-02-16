749a79296f443e7a58c23a92c1089ab057062b43
William Emfinger <waemfinger@gmail.com>
2026-02-16 16:41:24 -0600

    Update to UE 5.7, simplify map, add movable objects

--- Source/CameraCaptureExample.Target.cs
+++ Source/CameraCaptureExample.Target.cs
@@ -8,7 +8,7 @@ public class CameraCaptureExampleTarget : TargetRules
 	public CameraCaptureExampleTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 		// IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
 		ExtraModuleNames.Add("CameraCaptureExample");
 	}
--- Source/CameraCaptureExampleEditor.Target.cs
+++ Source/CameraCaptureExampleEditor.Target.cs
@@ -8,7 +8,7 @@ public class CameraCaptureExampleEditorTarget : TargetRules
 	public CameraCaptureExampleEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 		// IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
 		ExtraModuleNames.Add("CameraCaptureExample");
 	}
